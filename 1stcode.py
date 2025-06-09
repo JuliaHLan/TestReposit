c7398f8fe5e853e13a2a1b1c111a39685a190299
JuliaHLan <jkopeyko@gmail.com>
2025-06-09 10:58:25 -0700

    Add files via upload

---
+++ 1stcode.py
@@ -0,0 +1,8 @@
+Python 3.12.1 (v3.12.1:2305ca5144, Dec  7 2023, 17:23:38) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> ("Hello World!")
+'Hello World!'
+# Hello wrold!
+# This is a demonstration of the "print" function.
+input ("n\n Hit the Enter key to exit!")
+
